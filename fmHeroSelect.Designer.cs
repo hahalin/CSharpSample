a3304dd30f09c4c8a8b2e99bcc70d7a594e64623
hahalin <hahalin@gmail.com>
2016-03-01 11:34:33 +0800

    odify heroselect

--- fmHeroSelect.Designer.cs
+++ fmHeroSelect.Designer.cs
@@ -35,12 +35,12 @@
             this.rdiq = new System.Windows.Forms.RadioButton();
             this.rdfly = new System.Windows.Forms.RadioButton();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.lbname = new System.Windows.Forms.Label();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.rdwest = new System.Windows.Forms.RadioButton();
             this.rdeast = new System.Windows.Forms.RadioButton();
+            this.lbname = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -65,7 +65,7 @@
             this.lbslogan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.lbslogan.Font = new System.Drawing.Font("新細明體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.lbslogan.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.lbslogan.Location = new System.Drawing.Point(541, 541);
+            this.lbslogan.Location = new System.Drawing.Point(581, 409);
             this.lbslogan.Name = "lbslogan";
             this.lbslogan.Size = new System.Drawing.Size(37, 40);
             this.lbslogan.TabIndex = 1;
@@ -77,7 +77,7 @@
             this.rdpower.AutoSize = true;
             this.rdpower.BackColor = System.Drawing.Color.Transparent;
             this.rdpower.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.rdpower.Location = new System.Drawing.Point(115, 120);
+            this.rdpower.Location = new System.Drawing.Point(22, 107);
             this.rdpower.Name = "rdpower";
             this.rdpower.Size = new System.Drawing.Size(106, 37);
             this.rdpower.TabIndex = 2;
@@ -90,7 +90,7 @@
             this.rdmagic.AutoSize = true;
             this.rdmagic.BackColor = System.Drawing.Color.Transparent;
             this.rdmagic.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.rdmagic.Location = new System.Drawing.Point(230, 120);
+            this.rdmagic.Location = new System.Drawing.Point(137, 107);
             this.rdmagic.Name = "rdmagic";
             this.rdmagic.Size = new System.Drawing.Size(106, 37);
             this.rdmagic.TabIndex = 3;
@@ -103,7 +103,7 @@
             this.rdiq.AutoSize = true;
             this.rdiq.BackColor = System.Drawing.Color.Transparent;
             this.rdiq.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.rdiq.Location = new System.Drawing.Point(356, 120);
+            this.rdiq.Location = new System.Drawing.Point(263, 107);
             this.rdiq.Name = "rdiq";
             this.rdiq.Size = new System.Drawing.Size(106, 37);
             this.rdiq.TabIndex = 4;
@@ -116,7 +116,7 @@
             this.rdfly.AutoSize = true;
             this.rdfly.BackColor = System.Drawing.Color.Transparent;
             this.rdfly.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.rdfly.Location = new System.Drawing.Point(474, 120);
+            this.rdfly.Location = new System.Drawing.Point(381, 107);
             this.rdfly.Name = "rdfly";
             this.rdfly.Size = new System.Drawing.Size(106, 37);
             this.rdfly.TabIndex = 5;
@@ -126,39 +126,24 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox1.Location = new System.Drawing.Point(419, 225);
+            this.pictureBox1.Location = new System.Drawing.Point(355, 150);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(700, 441);
+            this.pictureBox1.Size = new System.Drawing.Size(725, 459);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
             // 
-            // lbname
-            // 
-            this.lbname.AutoSize = true;
-            this.lbname.BackColor = System.Drawing.Color.Transparent;
-            this.lbname.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.lbname.Location = new System.Drawing.Point(460, 189);
-            this.lbname.Name = "lbname";
-            this.lbname.Size = new System.Drawing.Size(147, 33);
-            this.lbname.TabIndex = 7;
-            this.lbname.Text = "選擇英雄";
-            // 
             // listBox1
             // 
-            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
             this.listBox1.BackColor = System.Drawing.Color.Navy;
             this.listBox1.ForeColor = System.Drawing.Color.White;
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 32;
-            this.listBox1.Location = new System.Drawing.Point(47, 179);
+            this.listBox1.Location = new System.Drawing.Point(12, 150);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(336, 484);
+            this.listBox1.Size = new System.Drawing.Size(329, 484);
             this.listBox1.TabIndex = 8;
             this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
             // 
@@ -166,9 +151,9 @@
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.button1.ForeColor = System.Drawing.SystemColors.Control;
-            this.button1.Location = new System.Drawing.Point(584, 107);
+            this.button1.Location = new System.Drawing.Point(504, 30);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(115, 59);
+            this.button1.Size = new System.Drawing.Size(188, 102);
             this.button1.TabIndex = 9;
             this.button1.Text = "招喚";
             this.button1.UseVisualStyleBackColor = false;
@@ -180,7 +165,7 @@
             this.groupBox1.Controls.Add(this.rdwest);
             this.groupBox1.Controls.Add(this.rdeast);
             this.groupBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.groupBox1.Location = new System.Drawing.Point(115, 12);
+            this.groupBox1.Location = new System.Drawing.Point(24, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(287, 77);
             this.groupBox1.TabIndex = 17;
@@ -208,17 +193,29 @@
             this.rdeast.Text = "東方";
             this.rdeast.UseVisualStyleBackColor = true;
             // 
+            // lbname
+            // 
+            this.lbname.AutoSize = true;
+            this.lbname.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.lbname.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbname.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbname.Location = new System.Drawing.Point(375, 162);
+            this.lbname.Name = "lbname";
+            this.lbname.Size = new System.Drawing.Size(47, 33);
+            this.lbname.TabIndex = 18;
+            this.lbname.Text = "__";
+            // 
             // fmHeroSelect
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::acerclass.Properties.Resources.game_eve_online_exodus_02_1600x1200;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1167, 693);
+            this.ClientSize = new System.Drawing.Size(1101, 660);
+            this.Controls.Add(this.lbname);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.lbname);
             this.Controls.Add(this.rdfly);
             this.Controls.Add(this.rdiq);
             this.Controls.Add(this.rdmagic);
@@ -230,7 +227,6 @@
             this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "fmHeroSelect";
             this.Text = "選取英雄";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
@@ -248,12 +244,12 @@
         private System.Windows.Forms.RadioButton rdiq;
         private System.Windows.Forms.RadioButton rdfly;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label lbname;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.RadioButton rdwest;
         private System.Windows.Forms.RadioButton rdeast;
+        private System.Windows.Forms.Label lbname;
 
     }
 }
