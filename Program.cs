9b40621f24988c1ade534834268fac4829a413da
hahalin <hahalin@gmail.com>
2016-03-01 11:36:35 +0800

    change defaultform

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace acerclass
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new fmherostart());
         }
     }
 }
